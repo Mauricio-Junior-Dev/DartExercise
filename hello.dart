7a1437f9f40812153dfb2c1366403be8a17d7629
MauMau221 <mauriciobgsp@gmail.com>
2026-06-24 22:03:20 -0300

    first commit

---
+++ hello.dart
@@ -0,0 +1,83 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  /* 
+  print('Digite escolha um operador \n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão');
+
+  double operador = double.parse(stdin.readLineSync()!);
+
+  print("Escreva dois números para realizar a operação escolhida: ");
+  double input1 = double.parse(stdin.readLineSync()!);
+  print("Digite o segundo número: ");
+  double input2 = double.parse(stdin.readLineSync()!);
+  double resultado = 0;
+
+  switch (operador) {
+    case 1 :
+      resultado = input1 + input2;
+      print("Resultado da soma: $resultado");
+      break;
+    case 2 :
+      resultado = input1 - input2;
+      print("Resultado: $resultado");
+      break;
+    case 3 :
+      resultado = input1 * input2;
+      print("Resultado: $resultado");
+      break;
+    case 4 :
+      resultado = input1 / input2;
+      print("Resultado: $resultado");
+      break;
+    default:
+      print("Numero inválido, tente um numero de 1 a 4.");
+  }
+*/
+
+  /* 
+  List<String> lista = ['maçã', 'banana', 'laranja', 'uva'];
+  lista.add('abacaxi');
+  lista.remove('banana');
+  print(lista.length);
+  print(lista);
+  */
+
+  /*
+  Map<int, String> user = {0: 'João', 1: 'idade', 2: 'dev'};
+
+  print(user);
+  */
+
+  /*
+  var nome = true;
+  if (nome == true) {
+    print("Nome é verdadeiro $nome");
+  } else if (nome == false) {
+    print("Nome é falso $nome");
+  }
+  */
+
+  /*
+  for (int i = 1; i <= 10; i++) {
+    print("Número: $i");
+  }
+  */
+
+  /*
+  print(
+    "Informe um número inteiro para calcular a soma dos numeros pares de 0 até o numero digitado: ",
+  );
+  double entrada = double.parse(stdin.readLineSync()!);
+  double count = 0;
+  for (var i = 0; i <= entrada; i++) {
+    if (entrada <= 0) {
+      print("Número inválido, tente novamente.");
+    }else if (i % 2 == 0) {
+      print("Número par: $i");
+      count += i;
+    }
+  }
+  print("Soma dos números pares: $count");
+  */
+}
